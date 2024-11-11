118fb44b1ea1c308844a7e3d5bbffdc4a2d78c45
fancy <sample@example.com>
2024-11-11 18:40:12 +0100

    Size chart created

--- frontend/OpenArtspaceGallery/src/components/AdminPanel/AdminPanelImagesSizesComponent.vue
+++ frontend/OpenArtspaceGallery/src/components/AdminPanel/AdminPanelImagesSizesComponent.vue
@@ -1,42 +1,62 @@
 <script setup lang="ts">
 
-import {onMounted, ref} from "vue";
-import {DecodeImageSizeDto, DecodeImagesSizesResponse, ImageSize} from "../../ts/imagesSizes/libImagesSizes.ts";
-import {WebClientSendGetRequest} from "../../ts/libWebClient.ts";
+  import {onMounted, ref} from "vue";
+  import {DecodeImageSizeDto, DecodeImagesSizesResponse, ImageSize} from "../../ts/imagesSizes/libImagesSizes.ts";
+  import {WebClientSendGetRequest} from "../../ts/libWebClient.ts";
 
-const imagesSizes = ref<ImageSize[]>([])
+  const imagesSizes = ref<ImageSize[]>([])
 
-onMounted(async () =>
-{
-  await OnLoad();
-})
+  onMounted(async () =>
+  {
+    await OnLoad();
+  })
 
-async function OnLoad()
-{
-  imagesSizes.value = await GetImagesSizesList()
-
-  for (let index = 0; index < imagesSizes.value.length; index++) {
-    alert(imagesSizes.value[index].name);
+  async function OnLoad()
+  {
+    imagesSizes.value = await GetImagesSizesList()
   }
-}
 
-async function GetImagesSizesList()
-{
-    return DecodeImagesSizesResponse((await (await WebClientSendGetRequest("/ImagesSizes/GetImagesSizesList")).json()))
-      .imagesSizes
-      .map(DecodeImageSizeDto)
-}
+  async function GetImagesSizesList()
+  {
+      return DecodeImagesSizesResponse((await (await WebClientSendGetRequest("/ImagesSizes/GetImagesSizesList")).json()))
+        .imagesSizes
+        .map(DecodeImageSizeDto)
+  }
 
 </script>
 
 <template>
 
-  <div class="admin-Panel-Images-Sizes-conteiner">
+  <div class="admin-panel-images-sizes-conteiner">
 
-    <div class="admin-Panel-Images-Sizes-header">
+    <div class="admin-panel-images-sizes-header">
       Images sizes
     </div>
+
+    <table>
+
+      <thead>
+        <tr>
+          <th>Size name</th>
+          <th>Width</th>
+          <th>Height</th>
+          <th>Custom</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(image) in imagesSizes" :key="image.id">
+          <td>{{ image.name }}</td>
+          <td>{{ image.width }}</td>
+          <td>{{ image.height }}</td>
+          <td>Empty</td>
+        </tr>
+      </tbody>
+
+    </table>
 
   </div>
 
 </template>
+
+<style src="public/css/adminPanel/adminPanelImagesSizes.less"/>
