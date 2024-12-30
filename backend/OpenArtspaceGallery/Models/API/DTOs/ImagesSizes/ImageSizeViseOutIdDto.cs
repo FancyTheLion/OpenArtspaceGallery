e65e37b303fd0fef580ee53c79df6a473c9cec5b
fancy <sample@example.com>
2024-12-30 12:42:14 +0100

    Implemented inheritance for ImageSizeDto

---
+++ backend/OpenArtspaceGallery/Models/API/DTOs/ImagesSizes/ImageSizeViseOutIdDto.cs
@@ -0,0 +1,44 @@
+using System.Text.Json.Serialization;
+using OpenArtspaceGallery.Helpers.Validators;
+
+namespace OpenArtspaceGallery.Models.API.DTOs.ImagesSizes;
+
+public class ImageSizeViseOutIdDto
+{
+    /// <summary>
+    /// Size name (large, medium and others)
+    /// </summary>
+    [JsonPropertyName("name")]
+    public String Name { get; private set; }
+
+    /// <summary>
+    ///  Image size (Width)
+    /// </summary>
+    [JsonPropertyName("width")]
+    public int Width { get; private set; }
+
+    /// <summary>
+    /// Image size (Height)
+    /// </summary>
+    [JsonPropertyName("height")]
+    public int Height { get; private set; }
+
+    public ImageSizeViseOutIdDto
+    (
+        string name,
+        int width,
+        int height
+    )
+    {
+        ImageSizeValidator.Validate(name, width, height);
+        
+        Name = name;
+        Width = width;
+        Height = height;
+    }
+    
+    public virtual ImageSize ToModel()
+    {
+        return new ImageSize(Guid.Empty, Name, Width, Height);
+    }
+}
