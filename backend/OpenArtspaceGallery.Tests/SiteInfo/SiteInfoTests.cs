36b4857b7ae1ea4ceffd734f7561a38e9e7644e9
fancy <sample@example.com>
2025-01-31 11:48:41 +0100

    Tests have been supplemented

--- backend/OpenArtspaceGallery.Tests/SiteInfo/SiteInfoTests.cs
+++ backend/OpenArtspaceGallery.Tests/SiteInfo/SiteInfoTests.cs
@@ -38,15 +38,36 @@ public class SiteInfoTests  : IClassFixture<TestsFactory<Program>>
     [InlineData("0.0    .2")]
     public async Task Get_GetBackendVersion_CheckVersion(string version)
     {
+        // Arrange
+        var expectedVersion = _expectedVersion;
+        
         // Act
         var response = await _client.GetAsync("/api/SiteInfo/GetBackendVersion");
+        
+        response.EnsureSuccessStatusCode();
         
         // Assert
         var responseData = JsonSerializer.Deserialize<BackendVersionResponse>(await response.Content.ReadAsStringAsync());
         
         Assert.NotNull(responseData); 
         Assert.NotNull(responseData.BackendVersion); 
-        Assert.Equal(_expectedVersion, responseData.BackendVersion.Version);
+        Assert.Equal(expectedVersion, responseData.BackendVersion.Version);
+        Assert.IsType<string>(responseData.BackendVersion.Version);
+    }
+
+    [Fact]
+    public async Task Get_GetBackendVersion_CheckTypeVersion()
+    {
+        // Act
+        var response = await _client.GetAsync("/api/SiteInfo/GetBackendVersion");
+        
+        response.EnsureSuccessStatusCode();
+        
+        // Assert
+        var responseData = JsonSerializer.Deserialize<BackendVersionResponse>(await response.Content.ReadAsStringAsync());
+        
+        Assert.NotNull(responseData); 
+        Assert.NotNull(responseData.BackendVersion); 
         Assert.IsType<string>(responseData.BackendVersion.Version);
     }
 
