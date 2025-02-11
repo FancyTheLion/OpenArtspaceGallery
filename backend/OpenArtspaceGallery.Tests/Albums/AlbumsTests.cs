cf0cc97a49f11e73de62db5d4cbbf79de5f529b6
fancy <sample@example.com>
2025-02-11 12:54:57 +0100

    Delete Moq

--- backend/OpenArtspaceGallery.Tests/Albums/AlbumsTests.cs
+++ backend/OpenArtspaceGallery.Tests/Albums/AlbumsTests.cs
@@ -3,7 +3,6 @@ using System.Net.Http.Json;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Moq;
 using Newtonsoft.Json;
 using OpenArtspaceGallery.Controllers;
 using OpenArtspaceGallery.Models;
