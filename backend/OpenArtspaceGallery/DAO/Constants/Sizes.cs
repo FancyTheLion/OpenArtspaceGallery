47426cda3634620ead55810c5e6233b7c1cb6da4
fancy <sample@example.com>
2025-04-16 18:42:47 +0200

    Sizes

---
+++ backend/OpenArtspaceGallery/DAO/Constants/Sizes.cs
@@ -0,0 +1,28 @@
+namespace OpenArtspaceGallery.DAO.Constants;
+
+public static class Sizes
+{
+    #region Thumbnail
+    
+    /// <summary>
+    /// Size id
+    /// </summary>
+    public static readonly Guid ThumbnailId = new Guid("ab2f7586-ef5f-4c54-b6e4-868fca797372");
+    
+    /// <summary>
+    /// Size name
+    /// </summary>
+    public const string ThumbnailName = "Thumbnail";
+    
+    /// <summary>
+    /// Width
+    /// </summary>
+    public const int ThumbnailWidth = 200; 
+    
+    /// <summary>
+    /// Height
+    /// </summary>
+    public const int ThumbnailHeight = 150; 
+    
+    #endregion
+}
