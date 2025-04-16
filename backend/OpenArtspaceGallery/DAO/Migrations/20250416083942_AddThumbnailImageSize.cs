2f1150a51da70631108ab31651ed5e521aeb8a77
fancy <sample@example.com>
2025-04-16 18:42:18 +0200

    Migrated default size for previews

---
+++ backend/OpenArtspaceGallery/DAO/Migrations/20250416083942_AddThumbnailImageSize.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using OpenArtspaceGallery.DAO.Constants;
+
+#nullable disable
+
+namespace OpenArtspaceGallery.DAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThumbnailImageSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData
+            (
+                "ImagesSizes",
+                new[] { "Id", "Name", "Width", "Height" },
+                new object[] { Sizes.ThumbnailId, Sizes.ThumbnailName, Sizes.ThumbnailWidth, Sizes.ThumbnailHeight }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
