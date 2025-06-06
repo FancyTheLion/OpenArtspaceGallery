aca515b326028ff6ae549aac7b4a50e181dd1974
fancy <sample@example.com>
2025-06-06 11:37:21 +0200

    Removed direct link to files, now preview is loaded via WebClientSendGetRequest

--- frontend/OpenArtspaceGallery/src/components/ImagesComponent/ThumbnailComponent.vue
+++ frontend/OpenArtspaceGallery/src/components/ImagesComponent/ThumbnailComponent.vue
@@ -1,19 +1,47 @@
 <script setup lang="ts">
 
 import {Image} from "../../ts/Images/libImages.ts";
+import {WebClientSendGetRequest} from "../../ts/libWebClient.ts";
+import {onMounted, ref} from "vue";
 
 const { image } = defineProps<{ image: Image }>()
+
+const thumbnail = ref<string | undefined >(undefined)
+
+onMounted(async () =>
+{
+  await OnLoad();
+})
+
+async function OnLoad()
+{
+  thumbnail.value = await GetPreview(image.thumbnailId)
+}
+
+async function GetPreview(previewId: string)
+{
+  const response = await WebClientSendGetRequest("/Files/" + previewId);
+
+  const blob = await response.blob();
+
+  return URL.createObjectURL(blob);
+
+}
 
 </script>
 
 <template>
-  <div class="thumbnail-container">
-    <img
-        :src="`http://localhost:5271/api/Files/${image.thumbnailId}`"
-        alt="Preview"
-        class="thumbnail-image"
 
-    />
-    <p class="thumbnail-name">{{ image.name }}</p>
+  <div class="thumbnail-container">
+
+    <img
+        :src="thumbnail ?? '/public/images/icons/imageNotFound.webp'"
+        alt="Preview"
+        class="thumbnail-image"/>
+
+    <div class="thumbnail-name">
+      {{ image.name }}
+    </div>
+
   </div>
 </template>
